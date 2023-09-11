0b1ba216fb79e8ac4c969d654b3a981c81e82c5e
felipeserikyaku <fcserikyaku@gmail.com>
2023-09-10 22:25:01 -0300

    foi alterado o caminho para onde o forms iria e o WebServlet do caminho para o login

--- src/main/java/com/example/javawebapp/LoginServlet.java
+++ src/main/java/com/example/javawebapp/LoginServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "LoginServlet", value = "/LoginServlet")
+@WebServlet(name = "login", value = "/login")
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
